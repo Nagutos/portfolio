f954201dd69c253974b697365b672fc3a4e78b44
Nagutos <78440072+Nagutos@users.noreply.github.com>
2025-09-10 13:03:25 +0200

    Fix github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
